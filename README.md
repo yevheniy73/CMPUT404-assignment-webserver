0320813e7c9be7d1749da8d6e50733017c22d2d6
yevheniy73 <kaznovsk@ualberta.ca>
2023-09-28 22:59:30 -0600

    Assignment finished

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ CMPUT404-assignment-webserver
 
 CMPUT404-assignment-webserver
 
+Yevhen Kaznovskyi
+September 2023
+
 See requirements.org (plain-text) for a description of the project.
 
 Make a simple webserver.
@@ -24,3 +27,8 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Resource Used Include:
+- https://www.geeksforgeeks.org/python-os-listdir-method/
+- https://stackoverflow.com/questions/4810927/how-to-go-up-a-level-in-the-src-path-of-a-url-in-html
+- https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+- https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection
--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 #  coding: utf-8 
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2023 Yevhen Kaznovskyi
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,8 +33,56 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        # Parse the incoming HTTP request
+        incoming_request = self.data.decode('utf-8').split('\r\n')[0]
+        type_of_request = incoming_request.split()[0]
+        og_path_request = incoming_request.split()[1]
+
+        # Construct the full path with the root
+        path_request = f"./www{og_path_request}"
+        mimetype = "text/css" if ".css" in path_request else "text/html"
+
+        # Check for '..' in the path to prevent a directory attack, include small explanation
+        if ".." in path_request:
+            content = "<h1>404 Not Found</h1><p>The page you are trying to access is not allowed.</p>"
+            message = f"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n{content}"
+            self.request.sendall(bytearray(message,'utf-8'))
+            return
+
+        # Handle redirects
+        if (og_path_request.endswith("/") == False) and ("." not in og_path_request[1:]) and (og_path_request != "/"):
+            message = f"HTTP/1.1 301 Moved Permanently\r\nLocation: {og_path_request}/\r\nConnection: close\r\n\r\n"
+            self.request.sendall(bytearray(message,'utf-8'))
+            return
+        
+        if type_of_request == "GET":
+            
+            if og_path_request.endswith("/"):
+                path_request += "index.html"
+
+            # Check if request is valid and exists
+            if os.path.exists(path_request):
+                
+                # Get the html content
+                with open(path_request, 'r', encoding='utf-8') as file:
+                    content = file.read()
+
+                # Send a 200 OK with the page content
+                message = f"HTTP/1.1 200 OK\r\nContent-Type: {mimetype}\r\nConnection: close\r\n\r\n{content}"
+                self.request.sendall(bytearray(message,'utf-8'))
+            else:
+                # Handle 404 Not Found if request not valid, include small explanation
+                content = "<h1>404 Not Found</h1><p>The page you are trying to access does not exist.</p>"
+                message = f"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n{content}"
+                self.request.sendall(bytearray(message,'utf-8'))
+        else:
+            # Handle 405 Method Not Allowed if the method is not GET, include small explanation
+            content = "<h1>405 Method Not Allowed</h1><p>The method you are attemping to use is not allowed.</p>"
+            message = f"HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html\r\nConnection: close\r\n\r\n{content}"
+            self.request.sendall(bytearray(message,'utf-8'))
+
+        return
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
